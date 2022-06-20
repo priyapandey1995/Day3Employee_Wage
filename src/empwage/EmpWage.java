aef50705dbb9f14d16cebdbf40d502d37b505389
priya <priyapandey8884.blr@gmail.com>
2022-06-20 20:55:34 +0530

    Checking Emplyoee is Present

--- src/empwage/EmpWage.java
+++ src/empwage/EmpWage.java
@@ -3,8 +3,17 @@ package empwage;
 public class EmpWage {
 
 	public static void main(String[] args) {
-		System.out.println("welcome to emplyoee wage");// TODO Auto-generated method stub
-
+		System.out.println("welcome to emplyoee wage");
+		// TODO Auto-generated method stub
+		int employeePresent = 1;
+		int isPresent =(int) Math.floor(Math.random() * 10 )%2;
+		if (isPresent == employeePresent)
+		
+			System.out.println("emplyoee is presennt");
+			else
+				System.out.println("not present");
 	}
+	}
+	
 
 }

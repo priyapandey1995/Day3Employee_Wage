f1f5d541f8fb5cdfb119411a060f256052d0394c
priya <priyapandey8884.blr@gmail.com>
2022-06-20 20:44:27 +0530

    welcome message

---
+++ src/empwage/EmpWage.java
@@ -0,0 +1,10 @@
+package empwage;
+
+public class EmpWage {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to emplyoee wage");// TODO Auto-generated method stub
+
+	}
+
+}

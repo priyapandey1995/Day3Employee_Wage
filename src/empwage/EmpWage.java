df6e97bd3b8ffc9e08c17e0e5fc4c15e31059575
Priya Pandey <96812522+priyapandey1995@users.noreply.github.com>
2022-06-22 23:25:26 +0530

    Update EmpWage.java

--- src/empwage/EmpWage.java
+++ src/empwage/EmpWage.java
@@ -3,48 +3,35 @@ package empwage;
 public class EmpWage {
 
 	public static void main(String[] args) {
-		System.out.println("welcome to emplyoee wage");
+		System.out.println("Welcome to Employee Wage");
 		// TODO Auto-generated method stub
-		//int employeePresent = 1;
+		int employeePresent = 1;
+		int employeePartTime = 2;
 		int wagePerHour = 20;
-		int empHours = 8;
-		int partTime = 4;
-		int empWage=0;
-		int totalSalary=0;
-	
-		int day = 1;
-		
-		for(day=1;day<=20;day++)
-		{
-		int isPresent =(int) Math.floor(Math.random() * 10 )%3;
-		switch (isPresent)
-		{
-			case 0:
-				System.out.println("Emplyoee is absent");
-				System.out.println("Emplyoee wage is 0");
-				break;
-			
-			case 1:
-				System.out.println("Emplyoee is present");
-				empHours=8;
-				break;
-			
-			case 2:
-				System.out.println("Emplyoee is working as part-Time");
-				partTime=4;
-				break;
-			
-			default:
-				System.out.println("invalid entry");
-				break;
-				
+		int numberOfHours = 8;
+		int numberOfHoursForPartTime = 4;
+		int workingDayInAMonth = 20;
+		int isPresent = (int) Math.floor(Math.random() * 3);
+		switch (isPresent) {
+		case 0:
+			System.out.println("Employee is Present");
+			System.out.println("Employee earn = " + numberOfHours * wagePerHour);
+			System.out.println("Employee earn per Month =" + numberOfHours * wagePerHour * workingDayInAMonth);
+		break;
+		case 1:
+			System.out.println("Employee is doing Part Time");
+			System.out.println("Employee earn = " + numberOfHoursForPartTime * wagePerHour);
+			System.out.println("Employee earn per Month =" + numberOfHoursForPartTime * wagePerHour * workingDayInAMonth);
+
+		break;
+		case 2:
+			System.out.println("Employee is not Present");
+			System.out.println("Employee earn = 0");
+			System.out.println("Employee earn = 0");
+		break;
 		}
-		    empWage = empHours * wagePerHour;
-		     totalSalary = totalSalary+empWage;
-			System.out.println(totalSalary);
-			
-		}
-		
+	}
+}
 
 		
 	

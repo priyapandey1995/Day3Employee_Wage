cec0b5538506e596c0218862fc27bb40421bf3fe
Priya Pandey <96812522+priyapandey1995@users.noreply.github.com>
2022-06-22 23:22:11 +0530

    Update EmpWage.java

--- src/empwage/EmpWage.java
+++ src/empwage/EmpWage.java
@@ -3,34 +3,36 @@ package empwage;
 public class EmpWage {
 
 	public static void main(String[] args) {
-		System.out.println("welcome to emplyoee wage");
+		System.out.println("Welcome to Employee Wage");
 		// TODO Auto-generated method stub
 		int employeePresent = 1;
+		int employeePartTime = 2;
 		int wagePerHour = 20;
-		int empHours = 8;
-		int partTime = 4;
-		int isPresent =(int) Math.floor(Math.random() * 10 )%3;
-		switch (isPresent)
-		{
-			case 0:
-				System.out.println("Emplyoee is absent");
-				break;
-			
-			case 1:
-				System.out.println("Emplyoee is present");
-				empHours=8;
-				break;
-			
-			case 2:
-				System.out.println("Emplyoee is working as part-Time");
-				partTime=4
-				break;
-			
-			default:
-				System.out.println("invalid entry");
-				break;
-		}
-		}
-	
+		int numberOfHours = 8;
+		int numberOfHoursForPartTime = 4;
+		int workingDayInAMonth = 20;
+		int isPresent = (int) Math.floor(Math.random() * 3);
+		switch (isPresent) {
+		case 0:
+			System.out.println("Employee is Present");
+			System.out.println("Employee earn = " + numberOfHours * wagePerHour);
+			int totalHourPerMonth = 1;
+			while (totalHourPerMonth <= 100) {
+				totalHourPerMonth++; }
+				System.out.println("Employee earn in a month =" + totalHourPerMonth * wagePerHour);
+			break;
+		case 1:
+			System.out.println("Employee is doing Part Time");
+			System.out.println("Employee earn = " + numberOfHoursForPartTime * wagePerHour);
+			System.out
+					.println("Employee earn per Month =" + numberOfHoursForPartTime * wagePerHour * workingDayInAMonth);
 
+			break;
+		case 2:
+			System.out.println("Employee is not Present");
+			System.out.println("Employee earn = 0");
+			System.out.println("Employee earn = 0");
+			break;
+		}
+	}
 }

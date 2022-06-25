ff818efc73d6b6eb237eaf7f8bba409f14011f3e
Priya Pandey <96812522+priyapandey1995@users.noreply.github.com>
2022-06-25 16:22:04 +0530

    Update EmpWage.java
    updated Employee wage

--- src/empwage/EmpWage.java
+++ src/empwage/EmpWage.java
@@ -10,29 +10,20 @@ public class EmpWage {
 		int wagePerHour = 20;
 		int numberOfHours = 8;
 		int numberOfHoursForPartTime = 4;
-		int workingDayInAMonth = 20;
 		int isPresent = (int) Math.floor(Math.random() * 3);
 		switch (isPresent) {
 		case 0:
 			System.out.println("Employee is Present");
 			System.out.println("Employee earn = " + numberOfHours * wagePerHour);
-			int totalHourPerMonth = 1;
-			while (totalHourPerMonth <= 100) {
-				totalHourPerMonth++; }
-				System.out.println("Employee earn in a month =" + totalHourPerMonth * wagePerHour);
-			break;
+		break;
 		case 1:
 			System.out.println("Employee is doing Part Time");
 			System.out.println("Employee earn = " + numberOfHoursForPartTime * wagePerHour);
-			System.out
-					.println("Employee earn per Month =" + numberOfHoursForPartTime * wagePerHour * workingDayInAMonth);
-
-			break;
+		break;
 		case 2:
 			System.out.println("Employee is not Present");
 			System.out.println("Employee earn = 0");
-			System.out.println("Employee earn = 0");
-			break;
+		break;
 		}
 	}
 }
